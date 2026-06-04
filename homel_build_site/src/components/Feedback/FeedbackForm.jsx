38b2a1ba35160f3a46c3e2281a5efe743eda8203
Dzmitry Astashonak <34admal@gmail.com>
2026-06-04 12:42:42 +0300

    feat: настроить точечные классы для подписей формы в FeedbackForm

--- homel_build_site/src/components/Feedback/FeedbackForm.jsx
+++ homel_build_site/src/components/Feedback/FeedbackForm.jsx
@@ -2,10 +2,18 @@ import styles from "./FeedbackForm.module.css";
 
 function FeedbackForm() {
   return (
-    <form className={styles.form}>
-     <span className={styles.title}>ЗАПОЛНИТЕ ФОРМУ</span>
-     <span className={styles.label}>Ваше имя</span> 
-    </form>
+ <form className={styles.form}>
+  <span className={styles.title}>ЗАПОЛНИТЕ ФОРМУ</span>
+
+  <span className={`${styles.label} ${styles.label_name}`}>Ваше имя</span>
+  <span className={styles.line}></span>
+
+  <span className={`${styles.label} ${styles.label_phone}`}>Номер телефона</span>
+  <span className={styles.line}></span>
+
+  <span className={`${styles.label} ${styles.label_text}`}>Опишите Вашу заявку</span>
+  <textarea className={styles.textarea}></textarea>
+</form>
   );
 }
 
