c1a64864de1fdb01fafbf94c90ffe898706aa344
Dzmitry Astashonak <34admal@gmail.com>
2026-06-04 20:11:44 +0300

    feat: стилизовать кнопку отправки и текстовое поле в FeedbackForm

--- homel_build_site/src/components/Feedback/FeedbackForm.jsx
+++ homel_build_site/src/components/Feedback/FeedbackForm.jsx
@@ -2,20 +2,27 @@ import styles from "./FeedbackForm.module.css";
 
 function FeedbackForm() {
   return (
- <form className={styles.form}>
-  <span className={styles.title}>ЗАПОЛНИТЕ ФОРМУ</span>
+    <form className={styles.form}>
+      <span className={styles.title}>ЗАПОЛНИТЕ ФОРМУ</span>
 
-  <span className={`${styles.label} ${styles.label_name}`}>Ваше имя</span>
-  <span className={styles.line}></span>
+      <span className={`${styles.label} ${styles.label_name}`}>Ваше имя</span>
+      <span className={styles.line}></span>
 
-  <span className={`${styles.label} ${styles.label_phone}`}>Номер телефона</span>
-  <span className={styles.line}></span>
+      <span className={`${styles.label} ${styles.label_phone}`}>
+        Номер телефона
+      </span>
+      <span className={styles.line}></span>
 
-  <span className={`${styles.label} ${styles.label_text}`}>Опишите Вашу заявку</span>
-  <textarea className={styles.textarea}></textarea>
-</form>
+      <span className={`${styles.label} ${styles.label_text}`}>
+        Опишите Вашу заявку
+      </span>
+      <textarea className={styles.textarea}></textarea>
+
+      <button type="submit" className={styles.submitBtn}>
+        Получить консультацию
+      </button>
+    </form>
   );
 }
 
 export default FeedbackForm;
-
