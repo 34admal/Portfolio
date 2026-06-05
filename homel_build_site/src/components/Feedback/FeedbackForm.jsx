135216560c5ed263e701938cacf24b26e29a443a
Dzmitry Astashonak <34admal@gmail.com>
2026-06-05 11:06:31 +0300

    feat: реализовать кастомный чекбокс с SVG-галочкой через useState

--- homel_build_site/src/components/Feedback/FeedbackForm.jsx
+++ homel_build_site/src/components/Feedback/FeedbackForm.jsx
@@ -3,25 +3,30 @@ import styles from "./FeedbackForm.module.css";
 function FeedbackForm() {
   return (
     <form className={styles.form}>
-      <span className={styles.title}>ЗАПОЛНИТЕ ФОРМУ</span>
+  <span className={styles.title}>ЗАПОЛНИТЕ ФОРМУ</span>
 
-      <span className={`${styles.label} ${styles.label_name}`}>Ваше имя</span>
-      <span className={styles.line}></span>
+  <input type="text" placeholder="Ваше имя" className={`${styles.input} ${styles.label_name}`} />
+  <span className={styles.line}></span>
 
-      <span className={`${styles.label} ${styles.label_phone}`}>
-        Номер телефона
-      </span>
-      <span className={styles.line}></span>
+  <input type="tel" placeholder="Номер телефона" className={`${styles.input} ${styles.label_phone}`} />
+  <span className={styles.line}></span>
 
-      <span className={`${styles.label} ${styles.label_text}`}>
-        Опишите Вашу заявку
-      </span>
-      <textarea className={styles.textarea}></textarea>
+  <span className={`${styles.label} ${styles.label_text}`}>Опишите Вашу заявку</span>
+  <textarea className={styles.textarea}></textarea>
 
-      <button type="submit" className={styles.submitBtn}>
-        Получить консультацию
-      </button>
-    </form>
+  <button type="submit" className={styles.submitBtn}>
+    Получить консультацию
+  </button>
+
+<label htmlFor="policy" className={styles.checkboxContainer}>
+  <input type="checkbox" id="policy" className={styles.checkboxHidden} />
+  <span className={styles.checkboxCustom}></span>
+  <span className={styles.checkboxText}>
+    Принимаю условия Политики конфиденциальности
+  </span>
+</label>
+</form>
+
   );
 }
 
