fee28a8ee6ae137ab699d18c13360fcac5d212b4
Dzmitry Astashonak <34admal@gmail.com>
2026-06-06 19:33:29 +0300

    style: зачистить дубликаты в Feedback.module.css и зафиксировать положение людей

--- homel_build_site/src/components/Feedback/Feedback.jsx
+++ homel_build_site/src/components/Feedback/Feedback.jsx
@@ -1,11 +1,14 @@
 import styles from "./Feedback.module.css";
 import FeedbackForm from "./FeedbackForm";
+import peopleImg from "./images/peopleImg.png"; 
 
 function Feedback() {
   return (
     <section className={styles.feedback}>
       <div className={styles.feedback__container}>
         <FeedbackForm />
+         <img src={peopleImg} alt="People" className={styles.people} />
+
       </div>
     </section>
   );
