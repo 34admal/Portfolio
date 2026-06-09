2370698b265caa23759199073b88eba5e52e2d2a
Dzmitry Astashonak <34admal@gmail.com>
2026-06-09 11:26:17 +0300

    feat: implement list layout structure for AboutUs left content

--- homel_build_site/src/components/AboutUs/AboutUs.jsx
+++ homel_build_site/src/components/AboutUs/AboutUs.jsx
@@ -4,10 +4,30 @@ function AboutUs() {
   return (
     <section className={styles.about}>
       <div className={styles.about__container}>
-        <h2>О нас</h2>
+        
+        {/* Объединяем всю левую часть в один блок */}
+        <div className={styles.leftContent}>
+          <span className={styles.title}>О НАС</span>
+          
+          <ul className={styles.list}>
+            <li className={styles.listItem}>Качественное проведение работ</li>
+            <li className={styles.listItem}>Обсуждение работы на каждом этапе</li>
+            <li className={styles.listItem}>Профессиональный инструмент</li>
+            <li className={styles.listItem}>Начинаем работу без аванса</li>
+            <li className={styles.listItem}>Доступные цены</li>
+          </ul>
+        </div>
+
+        {/* Правая часть (пока просто заглушка для круга) */}
+        <div className={styles.rightContent}>
+          {/* Тут будут наши круги */}
+        </div>
+
       </div>
     </section>
   );
 }
 
 export default AboutUs;
+
+
