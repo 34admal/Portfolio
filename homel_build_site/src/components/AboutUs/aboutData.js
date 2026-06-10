bce2e3aaf92c3f0e181971fa4b1e46eb9cee8792
Dzmitry Astashonak <34admal@gmail.com>
2026-06-10 20:19:08 +0300

    chore: isolate circle data array into aboutData file

---
+++ homel_build_site/src/components/AboutUs/aboutData.js
@@ -0,0 +1,5 @@
+export const circleData = [
+  { top: "более", number: "15", bottom: "лет успешной работы" },
+  { top: "более", number: "1000", bottom: "м2 готовых объектов" },
+  { top: "более", number: "500", bottom: "довольных клиентов" }
+];
