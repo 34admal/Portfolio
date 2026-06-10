6cd23ae927ff01a4d2154db539fdffd210ebd7c4
Dzmitry Astashonak <34admal@gmail.com>
2026-06-10 12:45:11 +0300

    style: implement homogenous item structure and clean up left content layout

--- homel_build_site/src/components/AboutUs/AboutUs.jsx
+++ homel_build_site/src/components/AboutUs/AboutUs.jsx
@@ -5,20 +5,35 @@ function AboutUs() {
     <section className={styles.about}>
       <div className={styles.about__container}>
         
-        {/* Объединяем всю левую часть в один блок */}
         <div className={styles.leftContent}>
           <span className={styles.title}>О НАС</span>
           
-          <ul className={styles.list}>
-            <li className={styles.listItem}>Качественное проведение работ</li>
-            <li className={styles.listItem}>Обсуждение работы на каждом этапе</li>
-            <li className={styles.listItem}>Профессиональный инструмент</li>
-            <li className={styles.listItem}>Начинаем работу без аванса</li>
-            <li className={styles.listItem}>Доступные цены</li>
-          </ul>
+          <div className={styles.item}>
+            <span className={styles.text}>Качественное проведение работ</span>
+            <span className={styles.line}></span>
+          </div>
+
+          <div className={styles.item}>
+            <span className={styles.text}>Обсуждение работы на каждом этапе</span>
+            <span className={styles.line}></span>
+          </div>
+
+          <div className={styles.item}>
+            <span className={styles.text}>Профессиональный инструмент</span>
+            <span className={styles.line}></span>
+          </div>
+
+          <div className={styles.item}>
+            <span className={styles.text}>Начинаем работу без аванса</span>
+            <span className={styles.line}></span>
+          </div>
+
+          <div className={styles.item}>
+            <span className={styles.text}>Доступные цены</span>
+            <span className={styles.line}></span>
+          </div>
         </div>
 
-        {/* Правая часть (пока просто заглушка для круга) */}
         <div className={styles.rightContent}>
           {/* Тут будут наши круги */}
         </div>
