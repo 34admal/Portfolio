e2f117faae927339cf5e2372f95b0bc3b66b03ec
Dzmitry Astashonak <34admal@gmail.com>
2026-06-11 11:03:51 +0300

    style: add styles for inner elements of about us circle

--- homel_build_site/src/components/AboutUs/AboutUs.jsx
+++ homel_build_site/src/components/AboutUs/AboutUs.jsx
@@ -1,48 +1,44 @@
+import { useState, useEffect } from "react";
+import { circleData } from "./aboutData";
 import styles from "./AboutUs.module.css";
 
 function AboutUs() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const currentData = circleData[currentIndex];
+
   return (
     <section className={styles.about}>
       <div className={styles.about__container}>
-        
         <div className={styles.leftContent}>
           <span className={styles.title}>О НАС</span>
-          
           <div className={styles.item}>
             <span className={styles.text}>Качественное проведение работ</span>
             <span className={styles.line}></span>
           </div>
-
           <div className={styles.item}>
             <span className={styles.text}>Обсуждение работы на каждом этапе</span>
             <span className={styles.line}></span>
           </div>
-
           <div className={styles.item}>
             <span className={styles.text}>Профессиональный инструмент</span>
             <span className={styles.line}></span>
           </div>
-
           <div className={styles.item}>
             <span className={styles.text}>Начинаем работу без аванса</span>
             <span className={styles.line}></span>
           </div>
-
           <div className={styles.item}>
             <span className={styles.text}>Доступные цены</span>
             <span className={styles.line}></span>
           </div>
         </div>
-
         <div className={styles.rightContent}>
-          {/* Тут будут наши круги */}
-        </div>
-
+  <span className={styles.circleTop}>{currentData.top}</span>
+  <span className={styles.circleNumber}>{currentData.number}</span>
+  <span className={styles.circleBottom}>{currentData.bottom}</span>
+</div>
       </div>
     </section>
   );
 }
-
 export default AboutUs;
-
-
