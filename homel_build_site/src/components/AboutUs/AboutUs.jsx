6c7f4595da88225aafcd17df7cc87a1d38f24845
Dzmitry Astashonak <34admal@gmail.com>
2026-06-14 18:52:08 +0300

    feat: finish pixel perfect layout for left content items in about us

--- homel_build_site/src/components/AboutUs/AboutUs.jsx
+++ homel_build_site/src/components/AboutUs/AboutUs.jsx
@@ -5,43 +5,51 @@ import styles from "./AboutUs.module.css";
 function AboutUs() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const currentData = circleData[currentIndex];
-   useEffect(() => {
-  const interval = setInterval(() => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % circleData.length);
-  }, 3000);
-  return () => clearInterval(interval);
-}, []);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % circleData.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
   return (
     <section className={styles.about}>
       <div className={styles.about__container}>
         <div className={styles.leftContent}>
           <span className={styles.title}>О НАС</span>
+
           <div className={styles.item}>
-            <span className={styles.text}>Качественное проведение работ</span>
+            <span className={`${styles.text} ${styles.itemText1}`}>
+              Качественное проведение работ
+            </span>
             <span className={styles.line}></span>
           </div>
-          <div className={styles.item}>
-            <span className={styles.text}>Обсуждение работы на каждом этапе</span>
-            <span className={styles.line}></span>
-          </div>
-          <div className={styles.item}>
-            <span className={styles.text}>Профессиональный инструмент</span>
-            <span className={styles.line}></span>
-          </div>
-          <div className={styles.item}>
-            <span className={styles.text}>Начинаем работу без аванса</span>
-            <span className={styles.line}></span>
-          </div>
-          <div className={styles.item}>
-            <span className={styles.text}>Доступные цены</span>
-            <span className={styles.line}></span>
-          </div>
+
+          
+         <div className={`${styles.item} ${styles.item2}`}>
+  <span className={`${styles.text} ${styles.text2}`}>Обсуждение работы на каждом этапе</span>
+  <span className={styles.line}></span>
+</div>
+         <div className={`${styles.item} ${styles.item3}`}>
+  <span className={`${styles.text} ${styles.text3}`}>Профессиональный инструмент</span>
+  <span className={styles.line}></span>
+</div>
+
+        <div className={`${styles.item} ${styles.item4}`}>
+  <span className={`${styles.text} ${styles.text4}`}>Начинаем работу без аванса</span>
+  <span className={styles.line}></span>
+</div>
+
+        <div className={`${styles.item} ${styles.item5}`}>
+  <span className={`${styles.text} ${styles.text5}`}>Доступные цены</span>
+  <span className={styles.line}></span>
+</div>
+
         </div>
         <div className={styles.rightContent}>
-  <span className={styles.circleTop}>{currentData.top}</span>
-  <span className={styles.circleNumber}>{currentData.number}</span>
-  <span className={styles.circleBottom}>{currentData.bottom}</span>
-</div>
+          <span className={styles.circleTop}>{currentData.top}</span>
+          <span className={styles.circleNumber}>{currentData.number}</span>
+          <span className={styles.circleBottom}>{currentData.bottom}</span>
+        </div>
       </div>
     </section>
   );
