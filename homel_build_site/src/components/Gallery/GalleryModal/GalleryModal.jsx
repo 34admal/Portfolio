a5acebe5d95fac28612ab167689b5918780abd45
Dzmitry Astashonak <34admal@gmail.com>
2026-06-24 11:08:45 +0300

    feat: create dedicated GalleryModal directory and initialize base component layout

---
+++ homel_build_site/src/components/Gallery/GalleryModal/GalleryModal.jsx
@@ -0,0 +1,13 @@
+import styles from "./GalleryModal.module.css";
+
+function GalleryModal() {
+  return (
+    <div className={styles.modalOverlay}>
+      <div className={styles.modalWindow}>
+        {/* Контент модального окна будет здесь */}
+      </div>
+    </div>
+  );
+}
+
+export default GalleryModal;
