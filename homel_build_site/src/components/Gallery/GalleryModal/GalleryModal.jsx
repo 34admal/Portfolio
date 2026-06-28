b64a84f282a8a8e353905f6d9502e389d9717ec8
Dzmitry Astashonak <34admal@gmail.com>
2026-06-28 13:52:10 +0300

    feat: import and structure static roof images into local array

--- homel_build_site/src/components/Gallery/GalleryModal/GalleryModal.jsx
+++ homel_build_site/src/components/Gallery/GalleryModal/GalleryModal.jsx
@@ -1,6 +1,15 @@
 
 
 import styles from "./GalleryModal.module.css";
+import roofImg1 from "./images/roofImg1.png";
+import roofImg2 from "./images/roofImg2.png";
+import roofImg3 from "./images/roofImg3.png";
+import roofImg4 from "./images/roofImg4.png";
+import roofImg5 from "./images/roofImg5.png";
+import roofImg6 from "./images/roofImg6.png";
+
+const roofImages = [roofImg1, roofImg2, roofImg3, roofImg4, roofImg5, roofImg6];
+
 
 function GalleryModal({ isOpen, title }) {
   if (!isOpen) return null;
@@ -10,6 +19,10 @@ function GalleryModal({ isOpen, title }) {
       <div className={styles.modalWindow}>
         <button className={styles.closeButton}>✕</button>
         <h2 className={styles.modalTitle}>{title || "КРОВЛЯ"}</h2>
+
+        <div className={styles.photoGrid}>
+          
+        </div>
       </div>
     </div>
   );
