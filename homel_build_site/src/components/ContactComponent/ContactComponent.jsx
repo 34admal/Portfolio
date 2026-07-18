14438617eec23a7808b6c8d5b0014cc0caf976b6
Dzmitry Astashonak <34admal@gmail.com>
2026-07-18 10:16:38 +0300

    feat(contact): refactor layout layout to flexbox columns using native img element

--- homel_build_site/src/components/ContactComponent/ContactComponent.jsx
+++ homel_build_site/src/components/ContactComponent/ContactComponent.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import css from './ContactComponent.module.css';
+import bgWorkers from './images/bg-workers.png';
 
-export default function ContactComponent() {
-   return (
+function ContactComponent() {
+  return (
     <section className={css.wrapper}>
       <span className={css.title}>СВЯЖИТЕСЬ С НАМИ ИЛИ ОСТАВЬТЕ ЗАЯВКУ</span>
-
-       <div className={css.contentBlock}>
-        {/* Чистый декоративный слой рабочих через CSS */}
-        <div className={css.workers} />
+      
+      <div className={css.contentBlock}>
+        <div className={css.workersColumn}>
+          <img src={bgWorkers} className={css.workersImg} alt="Рабочие" />
+        </div>
+        
+        <div className={css.formColumn}>
+          {/* Контент формы */}
+        </div>
       </div>
     </section>
   );
 }
+
+export default ContactComponent;
