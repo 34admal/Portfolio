8e94dff1ced44d2c1b4c5990ee0a4f79e864e7a1
Dzmitry Astashonak <34admal@gmail.com>
2026-09-20 19:50:14 +0300

    refactor: adjust grid alignment and hero section layout for 1920px viewports

--- homel_build_site/src/App.js
+++ homel_build_site/src/App.js
@@ -4,13 +4,10 @@ import logo from "./logo.svg";
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-
-
-
 import Layout from "./components/Layout";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
-import  HowWeWorkSection from "./components/HowWeWorkSection";
+import HowWeWorkSection from "./components/HowWeWorkSection";
 import OurServices from "./components/OurServices/OurServices";
 import PromoBlock from "./components/PromoBlock/PromoBlock";
 import Feedback from "./components/Feedback/Feedback";
@@ -20,37 +17,24 @@ import PriceComponent from "./components/PriceComponent/PriceComponent";
 import ContactComponent from "./components/ContactComponent/ContactComponent";
 import FooterComponent from "./components/FooterComponent/FooterComponent";
 
-
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Layout>
+     
         <Header />
         <HeroSection />
-        <HowWeWorkSection/>
-        <OurServices/>
-        <PromoBlock/>
-        <Feedback/>
-        <AboutUs/>
-        <Gallery/>
-        <PriceComponent/>
-        <ContactComponent/>
-        <FooterComponent/>
-      </Layout>
-
-      {/* <Router>
-        
-        <Routes>
-           <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<About />} /> 
-        
-        </Routes>
-        
-         <Main/>
-        <Footer/> 
-      </Router> */}
+        <HowWeWorkSection />
+        <OurServices />
+        <PromoBlock />
+        <Feedback />
+        <AboutUs />
+        <Gallery />
+        <PriceComponent />
+        <ContactComponent />
+        <FooterComponent />
+      
     </>
   );
 }
